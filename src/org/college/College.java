b83be2477283ef582ea08bbd37532fc89e1fa95c
shyammohan123 <shyammohan123@gmail.com>
2022-11-19 23:33:15 +0530

    Master code

---
+++ src/org/college/College.java
@@ -0,0 +1,12 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		// TODO Auto-generated method stub
+System.out.println("college name is dscewt");
+	}
+public void studentDept() {
+	// TODO Auto-generated method stub
+System.out.println("studentDept is hhh");
+}
+}
